7e2aebd8c0a792b88db315b07ab14f89bc46ea83
BILIMOLINGBOT <javlon5688@gmail.com>
2025-06-12 15:03:43 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-import { initUser, userData } from './auth.js';
+import { initUser } from './auth.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const user = await initUser();
 
   document.getElementById('userName').textContent = user.name;
-  document.getElementById('userPhoto').src = user.photo || 'default.jpg';
-  document.getElementById('userPhoto').onerror = () => {
-    document.getElementById('userPhoto').src = 'default.jpg';
+  document.getElementById('userPoints').textContent = `Ballar: ${user.points || 0}`;
+  const userPhoto = document.getElementById('userPhoto');
+  userPhoto.src = user.photo || 'default.jpg';
+  userPhoto.onerror = () => {
+    userPhoto.src = 'default.jpg';
   };
 });
