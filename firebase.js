4cb57832494071be178a354ba75f335d00ed9722
BILIMOLINGBOT <javlon5688@gmail.com>
2025-06-15 17:59:56 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,30 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth, signInWithCredential } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Firebase SDK funksiyalarini import qilish
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+// Firebase konfiguratsiyasi – sizning loyihangizga mos
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyD0gBUJNcrgvvntcrfKK7Ky8t6_9Qb96Io",
   authDomain: "bilimilova-64833.firebaseapp.com",
+  databaseURL: "https://bilimilova-64833-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "bilimilova-64833",
   storageBucket: "bilimilova-64833.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  messagingSenderId: "890689414502",
+  appId: "1:890689414502:web:8141d7aab413495c0c14a7",
+  measurementId: "G-ZBC8X1VVMZ"
 };
 
+// Firebase ilovasini ishga tushirish
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
 
-export { db, auth, doc, getDoc, setDoc, updateDoc };
+// Auth, Firestore va Analytics xizmatlarini ulash
+const auth = getAuth(app);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+// Tashqariga eksport qilish
+export { app, auth, db, analytics };
