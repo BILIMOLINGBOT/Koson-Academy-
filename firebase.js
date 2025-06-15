bd8e09450f0c0d43de1c76dc7fb622495bacefb1
BILIMOLINGBOT <javlon5688@gmail.com>
2025-06-15 17:31:32 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth, signInWithCredential } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "bilimilova-64833.firebaseapp.com",
+  projectId: "bilimilova-64833",
+  storageBucket: "bilimilova-64833.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth, doc, getDoc, setDoc, updateDoc };
