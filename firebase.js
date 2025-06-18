700bf347eff2ecf251f7e9d9e62b7630fffe97e9
BILIMOLINGBOT <javlon5688@gmail.com>
2025-06-18 07:22:44 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0gBUJNcrgvvntcrfKK7Ky8t6_9Qb96Io",
   authDomain: "bilimilova-64833.firebaseapp.com",
   databaseURL: "https://bilimilova-64833-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "bilimilova-64833",
-  storageBucket: "bilimilova-64833.appspot.com",
+  storageBucket: "bilimilova-64833.firebasestorage.app",
   messagingSenderId: "890689414502",
   appId: "1:890689414502:web:8141d7aab413495c0c14a7",
   measurementId: "G-ZBC8X1VVMZ"
@@ -15,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, doc, getDoc, setDoc, updateDoc };
+export { db };
