ec57d9c41b4b88c7edab6a54c5f0367e66cb4205
BILIMOLINGBOT <javlon5688@gmail.com>
2025-06-18 11:00:38 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0gBUJNcrgvvntcrfKK7Ky8t6_9Qb96Io",
   authDomain: "bilimilova-64833.firebaseapp.com",
   databaseURL: "https://bilimilova-64833-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "bilimilova-64833",
-  storageBucket: "bilimilova-64833.firebasestorage.app",
+  storageBucket: "bilimilova-64833.appspot.com",
   messagingSenderId: "890689414502",
   appId: "1:890689414502:web:8141d7aab413495c0c14a7",
   measurementId: "G-ZBC8X1VVMZ"
@@ -15,5 +16,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { app, db, auth };
