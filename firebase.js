507e7ae1e55d1324b6773a8e82096d0de5516c2e
BILIMOLINGBOT <javlon5688@gmail.com>
2025-06-21 18:33:55 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,26 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD0gBUJNcrgvvntcrfKK7Ky8t6_9Qb96Io",
   authDomain: "bilimilova-64833.firebaseapp.com",
   databaseURL: "https://bilimilova-64833-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "bilimilova-64833",
-  storageBucket: "bilimilova-64833.appspot.com",
+  storageBucket: "bilimilova-64833.firebasestorage.app",
   messagingSenderId: "890689414502",
   appId: "1:890689414502:web:8141d7aab413495c0c14a7",
   measurementId: "G-ZBC8X1VVMZ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, db, auth };
+// Export Firebase services
+export { app, analytics, auth, db, doc, getDoc, setDoc, updateDoc, signInAnonymously };
